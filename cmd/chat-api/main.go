50c5c9478225b9015777e153a5dc2afc9e7d8e00
Ilya Strelkovskiy <i.s.toaccept@gmail.com>
2024-02-10 22:57:55 +0300

    init commit

---
+++ cmd/chat-api/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("hello")
+
+    // todo: create config (cleanenv)
+    // todo: create logger (slog)
+    // todo: add docker
+    // todo: init postgresql
+    // todo: create router (chi)
+}
